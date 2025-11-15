e9799b7255125dabe3d03f0f27684dab9c5a0db6
tigistalexgech-git <tigistalexgech@gmail.com>
2025-11-15 13:10:04 +0300

    update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,10 +3,11 @@
 # Ask the user to input a number for which they want to see the multiplication table:
 
 #ask the user input 
-number = int(input("Enter a number to see its multiplication table "))
+number = int(input("Enter a number to see its multiplication table: "))
 
 
 # Generate and print the multiplication table from 1 to 10
 for i in range(1, 11):
-    result = i * number
-    print(f"{number} * {i} = {result}")
+
+    print(f"{number} * {i} = {number * i}")
+

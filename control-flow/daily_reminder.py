dcb1d4330c7deab1a1f6b99f448cb88be4d80882
tigistalexgech-git <tigistalexgech@gmail.com>
2025-11-15 08:38:01 +0300

    update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,30 +1,30 @@
-# Personal Daily Reminder
+# daily_reminder.py
 
-#Ask the user for a task , Ask for priority, Ask if time-bound
-while True:
-    task = input("Enter your task: ")
-    priority = input("Priority (high/medium/low): ").lower()
-    time_bound = input("Is it time-bound? (yes/no): ").lower()
+# Ask the user for a task
+task = input("Enter your task: ")
 
-    
-    #process using matchcase 
-    match priority:
-        case "high":
-            reminder = f"'{task}' is a high priority task"
-        case "medium":
-            reminder = f"'{task}' is a medium priority task"
-        case "low":
-            reminder = f"'{task} is a low priority task"
-        case _:
-            reminder = f"'{task}' has an unknown priority level"
-     
-    # Modify message if task is time-bound       
-    if time_bound == "yes":
-        reminder += " that requires immediate attention today!"
-    elif time_bound == "no":
-        reminder += ". Consider completing it when you have free time."
-   
-   # Print the final message
-    print("\nReminder: ", reminder)
-    
-    break
+# Ask for priority
+priority = input("Priority (high/medium/low): ").lower()
+
+# Ask if time-bound
+time_bound = input("Is it time-bound? (yes/no): ").lower()
+
+# Match Case to process priority
+match priority:
+    case "high":
+        reminder = f"'{task}' is a high priority task"
+    case "medium":
+        reminder = f"'{task}' is a medium priority task"
+    case "low":
+        reminder = f"'{task}' is a low priority task"
+    case _:
+        reminder = f"'{task}' has an unknown priority level"
+
+# Modify message if task is time-bound
+if time_bound == "yes":
+    reminder += " that requires immediate attention today!"
+    print("\nReminder:", reminder)
+else:
+    reminder += ". Consider completing it when you have free time."
+    print("\nNote:", reminder)
+ 

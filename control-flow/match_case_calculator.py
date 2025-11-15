4d27cdef39a5a05221a447013ab74af0e1b557f2
tigistalexgech-git <tigistalexgech@gmail.com>
2025-11-15 12:57:05 +0300

    update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,34 +1,31 @@
 #Simple Calculator with Match Case
 
 #Prompt user for numbers
-try:
-    first_number = int(input("Enter the first number : "))
-    second_number = int(input("Enter the second number:"))
-except ValueError:
-        print("Invalid input. Please enter numbers.")
-        exit()
 
-operation = input("Choose the operation (+, -, *, /):")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+operation = input("Choose the operation (+, -, *, /): ")
 
 # Initialize result
-result = None
 
 # Match Case to perform calculation
 match operation:
-   case "+":
-        result = first_number + second_number
-   case "-":
-        result = first_number - second_number
-   case "*":
-          result = first_number * second_number
-   case "/":
-          if second_number != 0:
-                result = first_number / second_number
-          else:
-                print("Error:Devision by zero is not allowed.")
-   case _:
-            print("Invalid operation.")
-        
-if result != 0:
-      print(f"Result: {result}")
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+            exit()
+        result = num1 / num2
+    case _:
+        print("Invalid operation.")
+        exit()
+
+print(f"The result is {result}.")
+
    

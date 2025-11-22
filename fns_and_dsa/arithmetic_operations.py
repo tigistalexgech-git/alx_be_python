c81451937c90d70da4c1f34acc8b7c10056c705e
tigistalexgech-git <tigistalexgech@gmail.com>
2025-11-22 13:53:47 +0300

    update arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,21 @@
-# arithmetic_operations.py
-
 def perform_operation(num1: float, num2: float, operation: str):
+    """
+    Performs basic arithmetic operations based on the given operation.
+    """
 
-    """Performs basic arithmetic operations based on the given operation string.
-
-    Parameters:
-        num1 (float), num2 (float),operation (str): The operation to perform ('add', 'subtract', 'multiply', 'divide')."""
-    if operation == 'add':
+    if operation == "add":
         return num1 + num2
-    elif operation == 'subtract':
+
+    elif operation == "subtract":
         return num1 - num2
-    elif operation == 'multiply':
+
+    elif operation == "multiply":
         return num1 * num2
-    elif operation == 'divide':
+
+    elif operation == "divide":
         if num2 == 0:
             return "Error: Division by zero"
         return num1 / num2
+
     else:
         return "Error: Invalid operation"

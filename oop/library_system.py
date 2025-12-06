2cbb804bd0466cf1eedf97abbf202e5071318877
tigistalexgech-git <tigistalexgech@gmail.com>
2025-12-06 17:15:10 +0300

    library_system

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,7 +4,8 @@ class Book:
     def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
-    def get_details(self):
+
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
@@ -12,7 +13,7 @@ class EBook(Book):
          super().__init__(title, author)
          self.file_size = file_size
     
-     def get_details(self):
+     def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -20,7 +21,7 @@ class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:  
@@ -32,4 +33,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            print(book)
